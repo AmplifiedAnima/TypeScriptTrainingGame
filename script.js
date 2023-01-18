5ef0c86e0d643b553864015e72ba9dbddc8db7d9
AmplifiedAnima <czupamichal@gmail.com>
2023-01-18 13:23:10 +0100

    next commit

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ var batonik4 = document.getElementById('batonik4')
 var batonik5 = document.getElementById('batonik5')
 var batonik6 = document.getElementById('batonik6')
 
-var wordsForTheGame= ['red','blue','yellow','orange','green','black'];
 
-const randomWord=Math.floor(Math.random() * 6);
+let wordsForTheGame= ['red','blue','yellow','orange','green','black'];
 
-var wordToClick = wordsForTheGame[randomWord];
+let wordToClick = wordsForTheGame[Math.floor(Math.random() * 6)];
+let word2ClickDisplay=document.getElementById('word2Click')
+word2ClickDisplay.innerText= wordToClick;
 
 var message4Player= document.getElementById('Message4player')
-
-let word2ClickDisplay=document.getElementById('word2Click')
-
 
 batonik1.style.backgroundColor=wordsForTheGame[0]
 batonik2.style.backgroundColor=wordsForTheGame[1]
@@ -28,28 +26,28 @@ batonik5.style.backgroundColor=wordsForTheGame[4]
 batonik6.style.backgroundColor=wordsForTheGame[5]
 
 const batons= document.querySelectorAll('.baton');
-batons.forEach(btn =>{
-    btn.style.backgroundColor[randomWord]
-})
+
 var scoreCard =document.getElementById('result')
-
+function shuffleColors(){
+   word2ClickDisplay.innerText =wordsForTheGame[Math.floor(Math.random() * 6)];
+}
 batons.forEach(btn => {
+    
     btn.addEventListener('click', clickEvent => {
-        
 
-        if(btn.style.backgroundColor===wordToClick){
-            if(score !=10){
-            word2ClickDisplay.innerText= wordsForTheGame[randomWord];}
+        if(btn.style.backgroundColor===  word2ClickDisplay.innerText){
             score++;
-            console.log(score);
             scoreCard.innerText= score;
             message4Player.innerText="That's the spirit!"
+            console.log(score);
+            shuffleColors();
         }else{
             message4Player.innerText='u missed cunt!';
-        }  if(score >= 10){
-            scoreCard.innerText='Won the game!';
-            }
-            ; 
+            shuffleColors();
+        }
+        if(score >= 10){
+            scoreCard.innerText='Won the game!';}
        
-    })
-});
+    });
+  
+ });
