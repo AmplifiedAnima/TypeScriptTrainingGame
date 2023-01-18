249ab499cc06099a735096b3c7b31b83f172ad23
AmplifiedAnima <czupamichal@gmail.com>
2023-01-18 15:22:35 +0100

    next

--- script.js
+++ script.js
@@ -2,52 +2,53 @@
 var score = 0;
 game_completed=false;
 
-var batonik1 = document.getElementById('batonik1')
-var batonik2 = document.getElementById('batonik2')
-var batonik3 = document.getElementById('batonik3')
-var batonik4 = document.getElementById('batonik4')
-var batonik5 = document.getElementById('batonik5')
-var batonik6 = document.getElementById('batonik6')
+function shuffleArray (arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
 
+let wordsForTheGame= ['red','blue','yellow','orange','green','black','aqua','gray','purple'];
 
-let wordsForTheGame= ['red','blue','yellow','orange','green','black'];
-
-let wordToClick = wordsForTheGame[Math.floor(Math.random() * 6)];
-let word2ClickDisplay=document.getElementById('word2Click')
-word2ClickDisplay.innerText= wordToClick;
-
-var message4Player= document.getElementById('Message4player')
-
-batonik1.style.backgroundColor=wordsForTheGame[0]
-batonik2.style.backgroundColor=wordsForTheGame[1]
-batonik3.style.backgroundColor=wordsForTheGame[2]
-batonik4.style.backgroundColor=wordsForTheGame[3]
-batonik5.style.backgroundColor=wordsForTheGame[4]
-batonik6.style.backgroundColor=wordsForTheGame[5]
 
 const batons= document.querySelectorAll('.baton');
-
 var scoreCard =document.getElementById('result')
+var message4Player= document.getElementById('Message4player')
+message4Player.innerText= 'Play the game!'
+let wordToClick = shuffleArray(wordsForTheGame)[0];
+let word2ClickDisplay=document.getElementById('word2Click')
+word2ClickDisplay.innerText= wordToClick
+batons.forEach(btn => {
+    btn.style.backgroundColor = shuffleArray(wordsForTheGame)[0];
+});
+
 function shuffleColors(){
-   word2ClickDisplay.innerText =wordsForTheGame[Math.floor(Math.random() * 6)];
-}
+
+    word2ClickDisplay.innerText =shuffleArray(wordsForTheGame)[0];
+        batons.forEach(btn => {
+            btn.style.backgroundColor = shuffleArray(wordsForTheGame)[0];
+        })
+    };
 batons.forEach(btn => {
     
     btn.addEventListener('click', clickEvent => {
-
+        
         if(btn.style.backgroundColor===  word2ClickDisplay.innerText){
             score++;
             scoreCard.innerText= score;
-            message4Player.innerText="That's the spirit!"
+            message4Player.innerText=`${word2ClickDisplay.innerText}- u scored!`
             console.log(score);
             shuffleColors();
         }else{
-            message4Player.innerText='u missed cunt!';
+            message4Player.innerText= `u missed cunt! check word to click!`;
             shuffleColors();
         }
         if(score >= 10){
-            scoreCard.innerText='Won the game!';}
+            scoreCard.innerText='Won the game!';
+            game_completed=true;}
        
     });
-  
+    
  });
